56bf8fc222f4154824d7bf83a50fd5cf890081a7
Vedaste Cyizere <revedaste@gmail.com>
2017-03-29 21:05:54 +0200

    added contacts and about us controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
 
-  get "/", to: 'images#index'
+  get "/", to: 'companies#index'
   get "/companies", to: 'companies#index'
   get "/companies/new", to: 'companies#new'
   post "/companies/search", to: 'companies#search'
